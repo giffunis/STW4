06359ded42037b7a91ee5ada74a6d6bc9deb6950
José Saúl Giffuni <giffunis@gmail.com>
2015-10-29 13:21:11 +0000

    Añadido el fichero static-server para que funcione

---
+++ static-server.js
@@ -0,0 +1,11 @@
+
+var static = require('node-static');
+
+var fileServer = new static.Server('./');
+
+console.log("Visit http://10.6.128.81:8080/index.html")
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(8080);
